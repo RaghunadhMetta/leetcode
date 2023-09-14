b1c898b119f174be3ccd27df9246e12d1eaa6964
Raghunadh Metta <raghunadhnits@gmail.com>
2023-09-13 23:37:19 -0500

    Time: 2 ms (8.24%), Space: 44.1 MB (57.56%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,40 +1,38 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count =0;
-        int i =0;
-        while(i<flowerbed.length){
-        System.out.println(i);
-            if(flowerbed.length==1 &&
-                flowerbed[i]==0){
-                count = count+1;    
-            }
-
-        else if(flowerbed[i]==0){
-
-            if(i==0 || i == flowerbed.length-2){
-                if(flowerbed[i]==0 && 
-                flowerbed[i+1]==0){
-                count=count+1;
-                }
-                
-            } else if(i==flowerbed.length-1){
-                
-            if(flowerbed[i]==0 && flowerbed[i-1]==0){
-                count=count+1; 
-            } 
-                
-             } else if(flowerbed[i]==0 && 
-                         flowerbed[i-1]==0 &&
-                         flowerbed[i+1]==0){
-                count=count+1; 
-                
-            }
+        if (n == 0) {
+            return true;
         }
-        i = i+2;
+        if (flowerbed.length == 1) {
+            return flowerbed[0] == 0 && n <= 1;
+        }
+        int count = 0;
+        int i = 0;
+        while (i < flowerbed.length) {
+
+            if (flowerbed[i] == 0) {
+                if (i == 0) {
+                    if (flowerbed[i + 1] == 0) {
+                        count = count + 1;
+                        i = i + 2;
+                        continue;
+                    }
+                } else if (i == flowerbed.length - 1) {
+                    if (flowerbed[i - 1] == 0) {
+                        count = count + 1;
+                        i = i + 2;
+                        continue;
+                    }
+                } else if (flowerbed[i - 1] == 0 &&
+                        flowerbed[i + 1] == 0) {
+                    count = count + 1;
+                    i = i + 2;
+                    continue;
+                }
+            }
+            i = i + 1;
         }
         System.out.println(count);
-        return count>=n;
-
-    
+        return count >= n;
     }
 }
