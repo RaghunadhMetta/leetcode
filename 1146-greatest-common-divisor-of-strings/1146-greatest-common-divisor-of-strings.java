41755760987774f8cb60b074ae25776817f5e27a
Raghunadh Metta <raghunadhnits@gmail.com>
2023-09-20 19:05:54 -0500

    Time: 1 ms (82.67%), Space: 41.2 MB (58.04%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,39 +1,16 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-
+        //Decider to make your code run fast.
+        if(!(str1+str2).equals(str2+str1)) {
+            return "";
+        }
         int gcdSize = 0;
         int minLength = Math.min(str1.length(),str2.length());
-
-        String expectedS1 = "";
-        String expectedS2 = "";
         for(int i=1;i<=minLength;i++){
             if(str1.length()%i==0 && str2.length()%i==0){
                 gcdSize=i;
             }
         }
-        // if(!str1.substring(0,gcdSize).equals(str2.substring(0,gcdSize))){
-        //     return "";
-        // }
-
-        int s1Generator = str1.length()/gcdSize;
-        int s2Generator = str2.length()/gcdSize;
-        
-        for(int i=0;i<s1Generator;i++){
-            expectedS1 = expectedS1 +str1.substring(0,gcdSize);
-
-        }
-        if(!expectedS1.equals(str1)){
-            return "";
-        }
-
-        for(int i=0;i<s2Generator;i++){
-            expectedS2 = expectedS2 +str1.substring(0,gcdSize);
-
-        }
-        if(!expectedS2.equals(str2)){
-            return "";
-        }
-
         return str1.substring(0,gcdSize);
     }
 }
